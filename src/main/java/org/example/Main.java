1c3985be8e36e4263092bf8ff1eaa6b29724bcba
manishkumar828386 <manikumar828386@gmail.com>
2023-05-13 22:15:01 +0530

    testng 1 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        @Test
+        void setup (){
+            System.out.println("this is setup class ");
+        }
+
+
+
     }
-}
+
+
